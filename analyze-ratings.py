231db42c8e5e5bce63d64fad4bffa9dfbd8cd603
Tamoghna Chowdhury <chowtam8991@gmail.com>
2017-08-09 22:02:42 +0530

    fixed rating_map for probability distribution

--- analyze-ratings.py
+++ analyze-ratings.py
@@ -10,7 +10,8 @@ RATINGS_FILE = "./DATABASE/ratings_cleaned.tsv"
 ANIME_FILE = "./DATABASE/anime_cleaned.tsv"
 OUTPUT_DIR = "./ANALYSIS"
 
-MAX_RATING = 10
+USER_KEY = 'user_id'
+MAX_RATING = 10.0
 
 def cluster_data(users, n_clusters):
     matrix = users.iloc[:, 1:]
@@ -62,8 +63,24 @@ class Baseline(Enum):
     MODE = 1
     MEDIAN = 2
     MEAN = 3
+    HALF = 4
 
-def rating_map(data, baseline=Baseline.MODE):
+def probability_map(data, baseline=Baseline.MODE):
+    """
+        Calculates a 2-dimensional probability distribution of the form:
+        Given that users liked genre 'x', what is the probability they will like genre 'y'?
+
+        Criterion for a user having liked a genre is whether the user has rated it more than some baseline value.
+
+        We use the definition of conditional probability to quantify this:
+        P(L_y|L_x) = P(L_y intersection L_x) / P(L_x) = n(L_y intersection L_x) / n(L_x),
+        where 'n(A)' is the size of the set 'A'
+
+        Parameters
+        __________
+        data : Pandas Dataframe
+        baseline : A member of the Baseline enum
+    """
     df = data.set_index('user_id')
     genres = list(df)[1:]
     genre_count = len(genres)
@@ -75,37 +92,35 @@ def rating_map(data, baseline=Baseline.MODE):
             return df[key].mean()
         elif baseline is Baseline.MEDIAN:
             return df[key].median()
+        elif baseline is Baseline.HALF:
+            return MAX_RATING / 2;
         else:
             raise ValueError("'baseline' must be one of 'mean', median' or 'mode'")
 
     genre_info = {genre: calc_baseline(genre) for genre in genres}
 
-    def count_users(a, b):
-        return len(df[a][df[a] >= genre_info[b]])
+    def users_who_liked(a, b):
+        items = df[a][df[a] >= genre_info[b]].index
+        return frozenset(items)
 
     def rated_users(a):
         return len(df[a][df[a] > 0.0])
-
-    def genre_quality(a):
-        return genre_info[a] / MAX_RATING
 
     corr_matrix = np.zeros((genre_count, genre_count))
 
     ordered_genres = list(sorted(genres, key=lambda x: genre_info[x], reverse=True))
     genres_with_indices = list(enumerate(ordered_genres))
     for i, a in genres_with_indices:
+        users_who_liked_a = users_who_liked(a, a)
+        num_users_who_liked_a = len(users_who_liked_a)
         for j, b in genres_with_indices:
-            p_a = (count_users(a, a) / rated_users(a)) * genre_quality(a)
-            p_b_a = (count_users(b, a) / rated_users(b)) * genre_quality(b)
-            # Probability can be maximally 1
-            p_b_given_a = min(1.0, p_b_a / p_a)
-            corr_matrix[i][j] = p_b_given_a
+            corr_matrix[i][j] = len(users_who_liked(b, b) & users_who_liked_a) / num_users_who_liked_a
 
     return pd.DataFrame(np.matrix(corr_matrix), columns=ordered_genres)
 
 def analyse_saved_data_new(df_file):
     df = load_saved_database(df_file, preserve_anime_data=False)
-    c3 = rating_map(df)
+    c3 = probability_map(df)
     c3.to_csv("log1.txt",index=False)
     plot_correlation(c3,"rating-mapped-genres")
 
@@ -126,4 +141,4 @@ def analyse_saved_data(df_file):
     genre_relations = {**genre2cluster, **cluster2genre}
     print(genre_relations, file=open("log2.txt", 'w'))
 
-analyse_saved_data(RATINGS_FILE)
+analyse_saved_data_new(RATINGS_FILE)
